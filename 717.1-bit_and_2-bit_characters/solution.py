bf12a90c76b4152be2cd931714fa7e3c8bbe1768
NghiaVNM <duongphanhieunghia2003@gmail.com>
2026-06-02 11:08:29 +0700

    feat(717): add solution for 717.1-bit_and_2-bit_characters

---
+++ 717.1-bit_and_2-bit_characters/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+  def isOneBitCharacter(self, bits: List[int]) -> bool:
+    if len(bits) == 1:
+      return True
+    i = 0
+    while i < len(bits):
+      if bits[i] == 1:
+        i += 1
+      if i == len(bits) - 2:
+        return True
+      i += 1
+    return False
